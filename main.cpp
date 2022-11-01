ede3347ff6bea90252463c0ba9c513d110764ba6
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-01 14:30:02 +0100

    Compilation and checking the correct operation of the program

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 string temat, nick;
 string tresc[5];
 string odpA[5], odpB[5], odpC[5], odpD[5];
-string poprawna [5];
+string poprawna[5];
 string odpowiedz;
 int punkty=0;
 
@@ -20,10 +20,9 @@ int main()
     int nr_pytania=0;
 
     fstream plik;
-  //otwarcie pliku do oczytu
     plik.open("quiz.txt",ios::in);
 
-    if (plik.good()==flase)
+    if (plik.good()==false)
     {
         cout<<"Nie udalo sie otworzyc pliku!";
         exit(0);
@@ -33,15 +32,14 @@ int main()
     {
         switch(nr_linii)
         {
-            case 1: temat=linia;                  break;
-            case 2: nick=linia;                   break;
-            case 3: tresc[nr_pytania] = linia;    break;
-            case 4: odpA[nr_pytania] = linia;     break;
-            case 5: odpB[nr_pytania] = linia;     break;
-            case 6: odpC[nr_pytania] = linia;     break;
-            case 7: odpD[nr_pytania] = linia;     break;
-            // poprawna odpowiedz na zadane pytanie
-            case 8: poprawna[nr_pytania] = linia; break;
+            case 1: temat=linia;                     break;
+            case 2: nick=linia;                      break;
+            case 3: tresc[nr_pytania] = linia;       break;
+            case 4: odpA[nr_pytania] = linia;        break;
+            case 5: odpB[nr_pytania] = linia;        break;
+            case 6: odpC[nr_pytania] = linia;        break;
+            case 7: odpD[nr_pytania] = linia;        break;
+            case 8: poprawna[nr_pytania] = linia;    break;
         }
 
         if (nr_linii==8) {nr_linii=2; nr_pytania++;}
@@ -52,7 +50,7 @@ int main()
 
     for (int i=0; i<=4; i++)
     {
-        cout<<endl<<tresc[1]<<endl;
+        cout<<endl<<tresc[i]<<endl;
         cout<<"A. "<<odpA[i]<<endl;
         cout<<"B. "<<odpB[i]<<endl;
         cout<<"C. "<<odpC[i]<<endl;
@@ -61,9 +59,18 @@ int main()
         cout<<"Twoja odpowiedz: ";
         cin>>odpowiedz;
 
-        transform(odpowiedz.begin(),odpowiedz.end(),odpowiedz.begin(), ::tolwer);
+        transform(odpowiedz.begin(), odpowiedz.end(), odpowiedz.begin(), ::tolower);
+
+        if (odpowiedz==poprawna[i])
+        {
+            cout<<"Dobrze! Zdobywasz punkt!"<<endl;
+            punkty++;
+        }
+        else cout<<"Zle! Brak punktu! Poprawna odpowiedz: "<<poprawna[i]<<endl;
 
     }
+
+    cout<<"Koniec quizu. Zdobyte punkty: "<<punkty;
 
     return 0;
 }
