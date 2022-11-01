47630144703379a390f6360e2aec0f9393edd570
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-01 10:00:01 +0100

    Add a libray

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
+string temat, nick;
+string tresc[5];
+string odpA[5], odpB[5], odpC[5], odpD[5];
+string poprawna [5];
 
 int main()
 {
